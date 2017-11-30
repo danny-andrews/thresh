5502402ae9a25e2afecff73616ed6e2d6d9f7061
Danny Andrews <lunde.andrews@gmail.com>
2017-11-30 16:40:42 -0600

    wip

--- src/circleci-weigh-in.js
+++ src/circleci-weigh-in.js
@@ -121,6 +121,7 @@ export default opts => {
           ])
         );
     }).catch(err => {
+      console.log(err, 'heyo');
       if(R.type(Error, err)) config.logError(err);
       else config.logMessage(err.message);
 
