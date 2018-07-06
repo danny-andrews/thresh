ea1661714f6ab8edacb35ecadb56ed691ce005b3
Danny Andrews <lunde.andrews@gmail.com>
2018-07-06 01:37:20 -0500

    Truncate all GitHub PR status descriptions
    Fix #56.

--- src/effects/post-pr-status.js
+++ src/effects/post-pr-status.js
@@ -1,52 +1,75 @@
+import R from 'ramda';
+import ncurry from 'ncurry';
 import makeGitHubRequest from '../make-github-request';
-import getPrStatusPayload, {StatusStates} from '../core/get-pr-status-payload';
 import ReaderPromise from '../core/reader-promise';
+import {truncate} from '../shared';
+import formatAssetDiff from '../core/format-asset-diff';
 
-const postPrStatus = ({sha, body}) =>
-  ReaderPromise.fromReaderFn(config =>
-    makeGitHubRequest({
-      path: `repos/${config.repoOwner}/${config.repoName}/statuses/${sha}`,
-      fetchOpts: {
-        headers: {'Content-Type': 'application/json'},
-        method: 'POST',
-        body
-      }
-    }).run(config)
-  );
+const MAX_DESCRIPTION_LENGTH = 140;
+
+const PENDING_STATUS_TEXT =
+  'Calculating asset diffs and threshold failures (if any)...';
+
+const StatusStates = {
+  FAILURE: 'failure',
+  SUCCESS: 'success',
+  PENDING: 'pending',
+  ERROR: 'error'
+};
+
+const postPrStatus = ncurry(
+  ['sha', 'state', 'targetUrl', 'label', 'description'],
+  ({sha, state, targetUrl, label, description}) =>
+    ReaderPromise.fromReaderFn(config =>
+      // TODO: We should be injecting this dependency. It would make testing
+      //   much easier.
+      makeGitHubRequest({
+        path: `repos/${config.repoOwner}/${config.repoName}/statuses/${sha}`,
+        fetchOpts: {
+          // TODO: Should we default these headers in make-github-request?
+          headers: {'Content-Type': 'application/json'},
+          method: 'POST',
+          body: {
+            state,
+            targetUrl,
+            context: label,
+            description: truncate(
+              {maxSize: MAX_DESCRIPTION_LENGTH},
+              description
+            )
+          }
+        }
+      }).run(config)
+    )
+);
+
+export const postPendingPrStatus = postPrStatus({
+  state: StatusStates.PENDING,
+  description: PENDING_STATUS_TEXT
+});
+export const postErrorPrStatus = postPrStatus({state: StatusStates.ERROR});
+const postSuccessPrStatus = postPrStatus({state: StatusStates.SUCCESS});
+const postFailurePrStatus = postPrStatus({state: StatusStates.FAILURE});
 
 export const postFinalPrStatus = ({
-  sha,
   assetDiffs,
   thresholdFailures,
-  targetUrl,
-  label
-}) => postPrStatus({
-  sha,
-  body: getPrStatusPayload({
-    assetDiffs,
-    thresholdFailures,
-    targetUrl,
-    label
-  })
-});
+  ...rest
+}) => {
+  const formatMessages = R.join(' \n');
+  const successDescription = R.pipe(
+    R.toPairs,
+    R.map(([filename, assetDiff]) => formatAssetDiff({filename, ...assetDiff})),
+    formatMessages
+  )(assetDiffs);
+  const failureDescription = R.pipe(
+    R.map(R.prop('message')),
+    formatMessages
+  )(thresholdFailures);
 
-export const postPendingPrStatus = ({sha, targetUrl, label}) => postPrStatus({
-  sha,
-  body: {
-    state: StatusStates.PENDING,
-    targetUrl,
-    context: label,
-    description: 'Calculating asset diffs and threshold failures (if any)...'
-  }
-});
-
-export const postErrorPrStatus = ({sha, targetUrl, label, description}) =>
-  postPrStatus({
-    sha,
-    body: {
-      state: StatusStates.ERROR,
-      targetUrl,
-      context: label,
-      description
-    }
-  });
+  return (
+    R.isEmpty(thresholdFailures)
+      ? postSuccessPrStatus({description: successDescription})
+      : postFailurePrStatus({description: failureDescription})
+  )({...rest});
+};
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ export {FailureThresholdStategies, FAILURE_THRESHOLD_TARGET_ALL}
   from './core/constants';
 export {default as diffAssets} from './core/diff-assets';
 export {default as formatAssetDiff} from './core/format-asset-diff';
-export {default as getPrStatusPayload} from './core/get-pr-status-payload';
 export {default as getThresholdFailures} from './core/get-threshold-failures';
 export {failureThresholdListSchema, DEFAULT_FAILURE_THRESHOLD_STRATEGY}
   from './core/schemas';
