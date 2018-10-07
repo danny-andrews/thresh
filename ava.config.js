49dcc7f8d14a296b7feb2a40c1dd4e955a74d721
Danny Andrews <lunde.andrews@gmail.com>
2018-10-07 15:16:34 -0500

    Use lerna

--- ava.config.js
+++ ava.config.js
@@ -1,5 +1,8 @@
 export default {
-  files: ['src/**/__tests__/**/*.js', '!**/shared/**'],
+  files: [
+    'packages/*/src/**/__tests__/**/*.js',
+    '!packages/thresh/src/shared/**'
+  ],
   require: ['@babel/register'],
   failWithoutAssertions: false
 };
---
+++ babel.config.js
@@ -0,0 +1,24 @@
+/* eslint-disable import/no-commonjs */
+module.exports = api => {
+  api.cache.never();
+
+  return {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            node: '6.10'
+          }
+        }
+      ]
+    ],
+    plugins: [
+      '@babel/plugin-proposal-object-rest-spread',
+      [
+        '@babel/plugin-proposal-pipeline-operator',
+        {proposal: 'minimal'}
+      ]
+    ]
+  };
+};
